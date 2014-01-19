d56d335531ef75b6be7de07c7f128280d4b809b8
Lars Kellogg-Stedman <lars@oddbit.com>
2014-01-18 21:34:44 -0500

    logging and config file parsing

--- editserver.py
+++ editserver.py
@@ -5,14 +5,24 @@ import sys
 import argparse
 import tempfile
 import urlparse
-from contextlib import contextmanager
+import shlex
+import yaml
+import logging
 
 import gevent
 from gevent import monkey; monkey.patch_all()
 from gevent import subprocess
 import bottle
 
+default_config_file = os.path.join(
+        os.environ['HOME'],
+        '.config', 'chreditor.yml')
+
+default_edit_command = 'gvim -f --servername WEB --remote-tab-wait-silent'
+
 app = bottle.app()
+config = {}
+log = None
 
 @app.route('/status')
 def do_status():
@@ -22,25 +32,56 @@ def do_status():
 def do_edit():
     url = bottle.request.headers['x-url']
     host = urlparse.urlparse(url)[1].split(':')[0]
+    cmd = config.get('editor', default_edit_command)
 
-    with tempfile.NamedTemporaryFile(prefix='%s_' % host) as fd:
+    log.info('received editor request from %s', url)
+
+    with tempfile.NamedTemporaryFile(prefix='%s_' % host, delete=False) as fd:
         data_in = bottle.request.body.read()
         fd.write(data_in)
-        fd.seek(0)
-        subprocess.check_call(['gvim', '-f', '--servername', 'WEB',
-                         '--remote-tab-wait-silent', fd.name])
-        fd.seek(0)
+
+    try:
+        subprocess.check_call(shlex.split(cmd) + [fd.name])
+    except OSError as detail:
+        log.warn('failed to run editor: %s', detail)
+
+    with open(fd.name) as fd:
         data_out = fd.read()
         yield data_out
 
 def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument('--port', '-p', default='45068')
+    p.add_argument('--config', '-f',
+            default=default_config_file)
+    p.add_argument('--debug', action='store_true')
     return p.parse_args()
 
 def main():
+    global config
+    global log
+
     args = parse_args()
-    app.run(server='gevent', port=args.port)
+    
+    if args.debug:
+        loglevel = logging.DEBUG
+    else:
+        loglevel = logging.WARN
+
+    logging.basicConfig(
+            level=loglevel,
+            datefmt='%Y-%m-%d %H:%M:%S',
+            format='%(asctime)s %(message)s')
+
+    log = logging.getLogger('chreditor')
+
+    try:
+        with open(args.config) as fd:
+            config = yaml.load(fd)['chreditor']
+    except (IOError, KeyError):
+        pass
+
+    app.run(server='gevent', port=args.port, debug=args.debug)
 
 if __name__ == '__main__':
     main()
